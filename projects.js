8d4945b4d7e34b2e8b1bafb11e641391179b7f73
vasiaalehin-design <vasiaalehin@gmail.com>
2026-05-30 18:12:16 +0300

    1

--- projects.js
+++ projects.js
@@ -92,5 +92,6 @@ const projects = {
       { type: 'full',      imgs: ['img/TAVRIDA_sita/TAVRIDA_01.jpg'] },
       { type: 'full',      imgs: ['img/TAVRIDA_sita/TAVRIDA_02.jpg'] },
       { type: 'video',     imgs: ['img/TAVRIDA_sita/TAVRIDA_video.mp4'] },
-    ] },
+    ] }
    },
+
