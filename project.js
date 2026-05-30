32860e65d1a30d9d52dc16341b3a554c24f2d036
vasiaalehin-design <vasiaalehin@gmail.com>
2026-05-30 16:47:36 +0300

    1

--- project.js
+++ project.js
@@ -23,6 +23,14 @@ function render(id) {
       blocks += `<div class="block-full reveal"><video src="/${b.imgs[0]}" autoplay loop muted playsinline style="width:100%;display:block;"></video></div>`;
     } else if (b.type === 'video2col') {
       blocks += `<div class="block-2col reveal">${b.imgs.map(s=>`<video src="/${s}" autoplay loop muted playsinline style="width:100%;display:block;"></video>`).join('')}</div>`;
+    } else if (b.type === 'imgvideo') {
+      const left = b.imgs[0].endsWith('.mp4') 
+        ? `<video src="/${b.imgs[0]}" autoplay loop muted playsinline style="width:100%;display:block;"></video>`
+        : `<img src="/${b.imgs[0]}" loading="lazy">`;
+      const right = b.imgs[1].endsWith('.mp4')
+        ? `<video src="/${b.imgs[1]}" autoplay loop muted playsinline style="width:100%;display:block;"></video>`
+        : `<img src="/${b.imgs[1]}" loading="lazy">`;
+      blocks += `<div class="block-2col reveal">${left}${right}</div>`;
     } else if (b.type === '2col') {
       blocks += `<div class="block-2col reveal">${b.imgs.map(s=>`<img src="/${s}" loading="lazy">`).join('')}</div>`;
     } else if (b.type === '3col') {
